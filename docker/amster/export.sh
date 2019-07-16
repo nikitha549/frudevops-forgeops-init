82ecd481d3867a5f6b30b136d20e521f925fd050
Warren Strange <warren.strange@gmail.com>
2019-07-16 14:40:38 -0600

    Update amster export.sh to use the https url in case secure cookies are used. Update am authorized_keys to allow connection from non localhost IPs

--- docker/amster/export.sh
+++ docker/amster/export.sh
@@ -5,9 +5,10 @@ rm -fr /var/tmp/amster
 mkdir -p /var/tmp/amster
 
 # Create Amster export script.
+# Note the https: url should be used in case secure cookies are enabled
 cat > /tmp/do_export.amster <<EOF
-connect -k  /var/run/secrets/amster/id_rsa http://am/am
-export-config --path /var/tmp/amster
+connect -k  /var/run/secrets/amster/id_rsa https://$NAMESPACE.$SUBDOMAIN.$DOMAIN/am
+export-config --realms /  --path /var/tmp/amster
 :quit
 EOF
 
